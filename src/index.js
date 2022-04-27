a681a5eda21fff2889445aa8abac6ea121068ebd
Paweł Grajewski <unionproject88@gmail.com>
2022-04-27 18:54:03 +0200

    add HOOKstore

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./styles.css";
 import App from "./App";
 import ProductsProvider from "./context/products-context";
+import configureStore from "./hooks-store/products-store";
 
 ReactDOM.render(
   <ProductsProvider>
